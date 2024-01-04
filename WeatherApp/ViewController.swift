d5491064fdda972c46e2875b2b47c13c62a44373
Yago Cardoso <48061155+YagoCardoso@users.noreply.github.com>
2024-01-03 22:18:43 -0300

    setup view code

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -8,11 +8,64 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private lazy var customView: UIView = {
+        let view = UIView(frame: .zero)
+        view.backgroundColor = .black
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupView()
+      
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+    
+    private func setupView() {
+        view.backgroundColor = UIColor.red
+        
+        setHierarchy()
+        setConstraint()
+      
+    }
+    
+    private func setHierarchy(){
+        view.addSubview(customView)
+    }
+    
+    private func setConstraint() {
+        NSLayoutConstraint.activate([
+            customView.topAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.topAnchor,
+                constant: 100),
+            customView.leadingAnchor.constraint(equalTo:
+                                                    view.safeAreaLayoutGuide.leadingAnchor, constant: 50),
+            customView.trailingAnchor.constraint(equalTo:
+                                                    view.safeAreaLayoutGuide.trailingAnchor, constant: -50),
+            customView.bottomAnchor.constraint(equalTo:
+                                                    view.safeAreaLayoutGuide.bottomAnchor, constant: -100)
+        ])
+        
+    }
+    
 
 
 }
